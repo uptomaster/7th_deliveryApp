97a1298feb2d5656a28be5f55877e51688320073
Minseo_Joh <whalstj9827@naver.com>
2026-05-28 00:06:51 +0900

    feat: 장바구니 contest Api 연동

--- delivery-order-app/src/App.jsx
+++ delivery-order-app/src/App.jsx
@@ -1,10 +1,14 @@
 import { BrowserRouter } from 'react-router-dom'
 import Router from './routes/Router'
+import { CartProvider } from './context/CartContext' // 👈 1. 불러오기 추가
 
 function App() {
   return (
     <BrowserRouter>
-      <Router />
+      {/* 2. CartProvider로 Router 감싸기 */}
+      <CartProvider>
+        <Router />
+      </CartProvider>
     </BrowserRouter>
   )
 }
