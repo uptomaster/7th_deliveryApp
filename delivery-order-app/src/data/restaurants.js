a09fb1e3e8e336b5cd1cce05fd1677e235c32052
Minseo_Joh <whalstj9827@naver.com>
2026-05-27 14:11:05 +0900

    feat: 사진가져오기

--- delivery-order-app/src/data/restaurants.js
+++ delivery-order-app/src/data/restaurants.js
@@ -1,3 +1,12 @@
+import img1 from '../assets/images/왕꼬치.jpg'
+import img2 from '../assets/images/떡고치.webp'
+import img3 from '../assets/images/물떡꼬치.jpg'
+import img4 from '../assets/images/소떡소떡.jpg'
+import img5 from '../assets/images/소시지꼬치.jpg'
+import img6 from '../assets/images/오뎅꼬치.jpg'
+import img7 from '../assets/images/은행꼬치.jpg'
+import img8 from '../assets/images/꼬치전.jpg'
+
 export const restaurants = [
   {
     id: 1,
@@ -5,7 +14,7 @@ export const restaurants = [
     category: '분식',
     rating: 4.6,
     description: '다양한 꼬치 메뉴를 즐길 수 있는 꼬치 전문점',
-    image: '/src/assets/images/hero.png',
+    image: img1,
     menus: [
       {
         id: 1,
@@ -33,7 +42,7 @@ export const restaurants = [
     category: '분식',
     rating: 4.6,
     description: '다양한 꼬치 메뉴를 즐길 수 있는 꼬치 전문점',
-    image: '/src/assets/images/hero.png',
+    image: img2,
     menus: [
       {
         id: 1,
@@ -61,7 +70,7 @@ export const restaurants = [
     category: '기타',
     rating: 4.6,
     description: '다양한 꼬치 메뉴를 즐길 수 있는 꼬치 전문점',
-    image: '/src/assets/images/hero.png',
+    image: img3,
     menus: [
       {
         id: 1,
@@ -89,7 +98,7 @@ export const restaurants = [
     category: '분식',
     rating: 4.6,
     description: '다양한 꼬치 메뉴를 즐길 수 있는 꼬치 전문점',
-    image: '/src/assets/images/hero.png',
+    image: img4,
     menus: [
       {
         id: 1,
@@ -117,7 +126,7 @@ export const restaurants = [
     category: '기타',
     rating: 4.6,
     description: '다양한 꼬치 메뉴를 즐길 수 있는 꼬치 전문점',
-    image: '/src/assets/images/hero.png',
+    image: img5,
     menus: [
       {
         id: 1,
@@ -145,7 +154,7 @@ export const restaurants = [
     category: '분식',
     rating: 4.6,
     description: '다양한 꼬치 메뉴를 즐길 수 있는 꼬치 전문점',
-    image: '/src/assets/images/hero.png',
+    image: img6,
     menus: [
       {
         id: 1,
@@ -173,7 +182,7 @@ export const restaurants = [
     category: '기타',
     rating: 4.6,
     description: '다양한 꼬치 메뉴를 즐길 수 있는 꼬치 전문점',
-    image: '/src/assets/images/hero.png',
+    image: img7,
     menus: [
       {
         id: 1,
@@ -201,7 +210,7 @@ export const restaurants = [
     category: '분식',
     rating: 4.6,
     description: '다양한 꼬치 메뉴를 즐길 수 있는 꼬치 전문점',
-    image: '/src/assets/images/hero.png',
+    image: img8,
     menus: [
       {
         id: 1,
